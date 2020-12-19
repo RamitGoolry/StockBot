1b47c6b6c755fed936e8333a2308043f03a0c717
Ramit Goolry <rgoolry@ucsd.edu>
2020-12-20 02:18:05 +0530

    Removed __init__.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         del api
 
     ts = TimeSeries(key = api_key, output_format='pandas')
-    data, metadata = ts.get_intraday('GOOGL', interval = '1min', outputsize = 'full')
+    data, _ = ts.get_intraday('GOOGL', interval = '1min', outputsize = 'full')
     # data = data.head(100) # DEBUG
 
     # Columns : date, 1. open, 2. high, 3. low, 4. close, 5. volume
@@ -22,4 +22,4 @@ def main():
     plt.show()
 
 if __name__ == '__main__':
-    main() 
+    main()
